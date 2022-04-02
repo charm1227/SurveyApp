67356576d99174f9eaef83f3f40f3e3199d6bf19
Cody Harmon <charm.on1227@gmail.com>
2022-04-02 17:39:47 -0600

    added file reading

--- SurveyApp/app.js
+++ SurveyApp/app.js
@@ -75,8 +75,8 @@ app.get('/dashboard', (request, response) => {
         return;
     }
 
-    const mySurveys = GetSurveyData('data/my_surveys');
-    const publishedSurveys = GetSurveyData('data/published_surveys');
+    const mySurveys = getAllSurveyData('data/my_surveys');
+    const publishedSurveys = getAllSurveyData('data/published_surveys');
 
     response.render('dashboard', {data : {mySurveys, publishedSurveys}});
     response.end();
@@ -179,27 +179,74 @@ app.get('/downloadSurveyData/:surveyCode', (request, response) => {
 
     response.end();
     return;
-})
+});
 
-function GetSurveyData(directory) {
+app.get('/takeSurvey/:surveyCode', (request, response) => {
+    let code = request.params.surveyCode;
+    let survey = getSurveyData('data/published_surveys/' + code + '.txt')
+
+    response.end();
+    return;
+});
+
+function parseQuestion(questionLine) {
+    let questionParts = questionLine.split(',');
+
+    const type = questionParts[0].trim();
+    const ques = questionParts[1].trim();
+
+    let question = new Question(type, ques, null);
+    if(type == 'MC') {
+        let responses = [];
+        if(questionParts.length > 2) {
+            for(let i=2; i<questionParts.length; i++) {
+                responses.push(questionParts[i].trim());
+            }
+        }
+        question.responses = responses;
+    }
+    return question;
+}
+
+function parsePhoneNumbers(phoneNumbersLine) {
+    const phoneNumbers = phoneNumbersLine.split(',');
+    const trimmedPhoneNumbers = phoneNumbers.map(s => {
+        return s.trim();
+    });
+    return trimmedPhoneNumbers;
+}
+
+function getSurveyData(filePath) {
+    let file = fs.readFileSync(filePath, 'utf-8');
+    file = file.trim();
+    const content = file.split('\n');
+
+    const code = path.parse(filePath).name;
+    const name = content[0];
+    const phoneNumbers = parsePhoneNumbers(content[1]);
+    let questions = [];
+
+    for(let i=2; i<content.length; i++) {
+        let question = parseQuestion(content[i]);
+        questions.push(question);
+    }
+
+    let survey = new Survey(code, name, phoneNumbers, questions);
+    return survey;
+}
+
+function getAllSurveyData(directory) {
     let fileArray = [];
-    
-    try {
-        const files = fs.readdirSync(directory);
-        files.forEach(file => {
-            let surveyCode = path.parse(file).name;
-            let content = fs.readFileSync(directory + '/' + file, 'utf-8');
-            let lineEnd = content.indexOf('\n');
-            let surveyName = content.substring(0, lineEnd);
+    const files = fs.readdirSync(directory);
+    files.forEach(file => {
+        let surveyCode = path.parse(file).name;
+        let content = fs.readFileSync(directory + '/' + file, 'utf-8');
+        let lineEnd = content.indexOf('\n');
+        let surveyName = content.substring(0, lineEnd);
 
-            let fileData = new FileData(surveyCode, surveyName);
-            fileArray.push(fileData);
-        });
-    }
-    catch(exception) {
-        console.log(exception);
-    }
-
+        let fileData = new FileData(surveyCode, surveyName);
+        fileArray.push(fileData);
+    });
     return fileArray;
 }
 
@@ -210,6 +257,23 @@ function authenticated(request, response) {
         return false;
     }
     return true;
+}
+
+class Question {
+    constructor(type, question, responses) {
+        this.type = type;
+        this.question = question;
+        this.responses = responses;
+    }
+}
+
+class Survey {
+    constructor(code, name, phoneNumbers, questions) {
+        this.code = code;
+        this.name = name;
+        this.phoneNumbers = phoneNumbers;
+        this.questions = questions;
+    }
 }
 
 class FileData {
