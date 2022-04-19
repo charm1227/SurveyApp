fddf88482833aa9ce20cf1295a4199ce3791f0c1
Kai <98994629+FireOVA@users.noreply.github.com>
2022-04-19 14:56:52 -0600

    Update config.js

--- SurveyApp/config.js
+++ SurveyApp/config.js
@@ -1,15 +1,3 @@
-module.exports.READ_MAIL_CONFIG = {
-  imap: {
-    user: process.env.surveyapplication2022,
-    password: process.env.AdminHSB2022,
-    host: 'imap.gmail.com',
-    port: 993,
-    authTimeout: 10000,
-    tls: true,
-    tlsOptions: { rejectUnauthorized: false },
-   },
-};
-
 module.exports.SEND_MAIL_CONFIG = {
     service: 'gmail',
     auth: {
